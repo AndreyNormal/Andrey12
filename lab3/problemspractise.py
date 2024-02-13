128d9a68680fa425416719ad642fb47d19476599
AndreyNormal <69591303+AndreyNormal@users.noreply.github.com>
2024-02-13 11:51:05 +0600

    Add files via upload

---
+++ lab3/problemspractise.py
@@ -0,0 +1,60 @@
+#writer a python function to find the maximum of 3 numbers
+
+# def max_of_two(x, y):
+#     # Check if x is greater than y
+#     if x > y:
+#         # If x is greater, return x
+#         return x
+#     # If y is greater or equal to x, return y
+#     return y
+
+# # Define a function that returns the maximum of three numbers
+# def max_of_three(x, y, z):
+#     # Call max_of_two function to find the maximum of y and z,
+#     # then compare it with x to find the overall maximum
+#     return max_of_two(x, max_of_two(y, z))
+
+# # Print the result of calling max_of_three function with arguments 3, 6, and -5
+# print(max_of_three(3, 6, -5)) 
+
+# #2. Write a Python function to sum all the numbers in a list.
+# print(bool("abc"))
+# print(bool(123))
+# print(bool(["apple", "cherry", "banana"]))
+
+# def greet(first_name,last_name):
+#     print(f"Hi {first_name} {last_name}")
+#     print("Welcome")
+
+
+# greet("Mosh","HAsh")   
+
+#1-Perform a task
+#2-Return a value
+
+# def greet(name):
+#     print(f"hi {name}")
+
+# def get_greeting(name):
+#     return f"hi{name}"
+
+# message = get_greeting("Mosh")
+# file = open("content.txt","w")
+# file.write(message)
+
+# def greet(name):
+#     print(f"hi{name}")
+
+# print(greet("Mosh"))
+
+
+# def increment(number,by):
+#   return number + by
+
+# print(increment(2,by =1))
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
