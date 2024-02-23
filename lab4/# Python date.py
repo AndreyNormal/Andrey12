f4ef21e54a9dd2d70770607ea8151b7b20d69ca5
AndreyNormal <69591303+AndreyNormal@users.noreply.github.com>
2024-02-23 17:15:06 +0600

    Add files via upload

---
+++ lab4/# Python date.py
@@ -0,0 +1,44 @@
+# Python date
+# Write a Python program to subtract five days from current date.
+# from datetime import date, timedelta
+# datetime = date.today() - timedelta(5)
+# print("current date:",date.today())
+# print("5 current date: "),datetime()
+
+
+# from datetime import date, timedelta
+# datetime = date.today() - timedelta(5)
+# print('Current Date :',date.today())
+# print('5 days before Current Date :',datetime)
+
+
+
+# Write a Python program to print yesterday, today, tomorrow.
+# import datetime
+# today = datetime.datetime.today()
+# print("Today: " ,today)
+# yesterday = today-datetime.timedelta(days=1)
+# print("Yesterday: ",yesterday)
+# tomorrow = today+datetime.timedelta(days=1)
+# print("Tomorrow:",tomorrow)
+
+
+
+# Write a Python program to drop microseconds from datetime.
+# import datetime
+# dt = datetime.datetime.today().replace(microsecond=0)
+# print()
+# print(dt)
+# print()
+
+# Write a Python program to calculate two date difference in seconds.
+# from datetime import datetime, time
+# def date_diff_in_Seconds(deltatimet2, deltatimet1):
+#   timedelta = deltatimet2 - deltatimet1
+#   return timedelta.days * 24 * 3600 + timedelta.seconds
+# #Specified date
+# date1 = datetime.strptime('2015-01-01 01:00:00', '%Y-%m-%d %H:%M:%S')
+# #Current date
+# date2 = datetime.now()
+# print("\n%d seconds" %(date_diff_in_Seconds(date2, date1)))
+# print()
